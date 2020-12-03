456582d094c01f4a252a29b71b91584e7e71ef0a
Audiophonics <contact@audiophonics.fr>
2020-12-03 18:43:33 +0100

    Update install_oled.sh

--- volumio/install_oled.sh
+++ volumio/install_oled.sh
@@ -4,7 +4,7 @@ start_time="$(date +"%T")"
 this_directory=${PWD}
 
 cd oled
-printf "\n\n##### INSTANCE OF INSTALLATION OLED MOODE ####\n\n" >> install_log.txt
+printf "\n\n##### INSTANCE OF INSTALLATION OLED MOODE ####\n\n" >> "${this_directory}/log.txt"
 bash install.sh |& tee -a "${this_directory}/log.txt"
 cd ..
 
