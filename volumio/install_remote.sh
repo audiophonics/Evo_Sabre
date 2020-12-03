a767ea61a645bdaedfbe6b7eab43358d68e50790
Audiophonics <contact@audiophonics.fr>
2020-12-03 17:34:38 +0100

    Update install_remote.sh

--- volumio/install_remote.sh
+++ volumio/install_remote.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 start_time="$(date +"%T")"
-
+this_directory=${PWD}
 cd remote
 printf "\n\n##### INSTANCE OF INSTALLATION REMOTE CONTROL FOR VOLUMIO ####\n\n" >> install_log.txt
-bash install.sh |& tee -a install_log.txt
+bash install.sh |& tee -a "${this_directory}/log.txt"
 cd ..
 
 exit 0
