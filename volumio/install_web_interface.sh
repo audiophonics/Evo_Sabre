d6cf5287109687f7389840323390a38203dc1501
Audiophonics <contact@audiophonics.fr>
2020-12-03 13:38:45 +0100

    Add files via upload

--- volumio/install_web_interface.sh
+++ volumio/install_web_interface.sh
@@ -3,7 +3,7 @@
 start_time="$(date +"%T")"
 
 cd oled
-printf "\n\n##### INSTANCE OF INSTALLATION OLED VOLUMIO ####\n\n" >> install_log.txt
+printf "\n\n##### INSTANCE OF INSTALLATION WEB INTERFACE VOLUMIO ####\n\n" >> install_log.txt
 bash install.sh |& tee -a install_log.txt
 cd ..
 
