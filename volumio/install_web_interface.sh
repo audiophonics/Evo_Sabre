65c4db7e0fbc6f751eeb2299fe839a84e97cff3e
Audiophonics <contact@audiophonics.fr>
2020-12-03 17:35:44 +0100

    Update install_web_interface.sh

--- volumio/install_web_interface.sh
+++ volumio/install_web_interface.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 start_time="$(date +"%T")"
-
+this_directory=${PWD}
 cd apts_web_interface
 printf "\n\n##### INSTANCE OF INSTALLATION WEB INTERFACE VOLUMIO ####\n\n" >> install_log.txt
-bash install.sh |& tee -a install_log.txt
+bash install.sh |& tee -a "${this_directory}/log.txt"
 cd ..
 
 exit 0
