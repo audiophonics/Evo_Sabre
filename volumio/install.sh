99649d112abd9f23e2f529c2571ebf88021e748d
Audiophonics <contact@audiophonics.fr>
2020-12-03 17:36:28 +0100

    Update install.sh

--- volumio/install.sh
+++ volumio/install.sh
@@ -2,7 +2,7 @@
 
 start_time="$(date +"%T")"
 
-printf "\n\n#=#=#=#=#=#=#=#=#=# Installation of whole package (Volumio) \n\n" >> install_log.txt
+printf "\n\n#=#=#=#=#=#=#=#=#=# Installation of whole package (Volumio) \n\n" >> log.txt
 bash install_remote.sh &&
 bash install_web_interface.sh &&
 bash install_oled.sh &&
@@ -10,8 +10,8 @@ node enable_volumio_wizard.js &&
 
 # ---------------------------------------------------
 # Say something nice and exit
-printf "\n\n#=#=#=#=#=#=#=#=#=# \n"
-echo started at $start_time finished at "$(date +"%T")"
+printf "\n\n#=#=#=#=#=#=#=#=#=# \n" >> log.txt
+echo started at $start_time finished at "$(date +"%T")" 
 echo "You should reboot now. Enjoy your Evo Sabre and have a nice day."
 
 exit 0
