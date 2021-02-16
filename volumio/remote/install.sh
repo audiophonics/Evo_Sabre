fab191d8ef22dbd0422541bd70d8c53c73e1ea03
Audiophonics <contact@audiophonics.fr>
2021-02-16 15:53:20 +0100

    Add files via upload

--- volumio/remote/install.sh
+++ volumio/remote/install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 start_time="$(date +"%T")"
-
+echo "* Installing : Evo Sabre Remote"
+echo "" > install_log.txt
 # ---------------------------------------------------
 # Install C dependencies 
-apt-get install -y lirc
+apt-get install -y lirc > /dev/null 2>> install_log.txt
 
 # ---------------------------------------------------
 # Enable gpio-ir driver to allow hardware interfacing
 if ! grep -q "dtoverlay=gpio-ir,gpio_pin=4" "/boot/userconfig.txt"; then
     echo "dtoverlay=gpio-ir,gpio_pin=4"  >> /boot/userconfig.txt
 fi
-sudo rsync -a config/ /etc/lirc/
+rsync -a config/ /etc/lirc/
 
 # ---------------------------------------------------
 # Say something nice and exit
-printf "\n\n-----------------------------------------\n"
-echo started at $start_time finished at "$(date +"%T")"
-echo "You should reboot now. Enjoy your remote and have a nice day."
+echo "* End of installation : Evo Sabre Remote - reboot required"
+echo started at $start_time finished at "$(date +"%T")" >> install_log.txt
+exit 0
