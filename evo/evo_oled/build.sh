b1618c28ec4f392bf54a9a6087c67eee1e5d59e8
Audiophonics <webmaster@audiophonics.fr>
2024-01-26 16:06:54 +0100

    Update build.sh

--- evo/evo_oled/build.sh
+++ evo/evo_oled/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -f evo_oled.tcz
-chmod +x extension/usr/local/bin
+chmod +x extension/usr/local/bin/*
 tce-load -w squashfs-tools.tcz
 tce-load -li squashfs-tools.tcz
 mksquashfs extension evo_oled.tcz
