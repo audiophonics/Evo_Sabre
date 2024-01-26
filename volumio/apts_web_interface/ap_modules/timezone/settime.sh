e44b65208c338bf83c97df025916c92074816bf9
Audiophonics <webmaster@audiophonics.fr>
2024-01-26 13:51:12 +0100

    Delete volumio directory

--- volumio/apts_web_interface/ap_modules/timezone/settime.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo bash -c '
-echo -e "tzdata tzdata/Areas select '"$1"'\ntzdata tzdata/Zones/'"$1"' select '"$2"'" > /tmp/tz ; 
-debconf-set-selections /tmp/tz 2>/dev/null
-rm /etc/localtime /etc/timezone 2>/dev/null 
-dpkg-reconfigure -f non-interactive tzdata 2>/dev/null 
-cat /etc/timezone
-'
